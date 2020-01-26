51d1b722d29e6643bfdda1a8ae01c14cdedf8a02
Alex <chieftain.alex@gmail.com>
2020-01-26 16:44:15 +0000

    new folders for templates and databases

--- gwbbcode/constants.inc.php
+++ gwbbcode/constants.inc.php
@@ -4,13 +4,13 @@ $gwbbcode_version = '1.8.0.2';
 
 // DATABASE PATHS
 //  Skill ids to details
-define('SKILLS_PATH', GWBBCODE_ROOT.'/skill_db.php');
+define('SKILLS_PATH', GWBBCODE_ROOT.'/databases/skill_db.php');
 //  Skill names into ids
-define('SKILLNAMES_PATH', GWBBCODE_ROOT.'/skillname_db.php');
+define('SKILLNAMES_PATH', GWBBCODE_ROOT.'/databases/skillname_db.php');
 //  Skill shorthand names into full names
-define('SKILLABBRS_PATH', GWBBCODE_ROOT.'/abbr_db.php');
+define('SKILLABBRS_PATH', GWBBCODE_ROOT.'/databases/abbr_db.php');
 //  Skill pvp ids into pve ids
-define('SKILLIDSPVP_PATH', GWBBCODE_ROOT.'/skill_pvpids_db.php');
+define('SKILLIDSPVP_PATH', GWBBCODE_ROOT.'/databases/skill_pvpids_db.php');
 
 // INCLUDE PATHS
 //  Used in common.inc.php
@@ -18,6 +18,6 @@ define('CONFIG_PATH', GWBBCODE_ROOT.'/config.inc.php');
 
 //  Used in gwbbcode.inc.php
 define('GWBBCODE_IMG_PATH', GWBBCODE_ROOT);
-define('TEMPLATE_PATH', GWBBCODE_ROOT.'/gwbbcode.tpl');
-define('GWBB_DYNAMIC_BODY', GWBBCODE_ROOT.'/dynamic_body.tpl');
+define('TEMPLATE_PATH', GWBBCODE_ROOT.'/templates/gwbbcode.tpl');
+define('GWBB_DYNAMIC_BODY', GWBBCODE_ROOT.'/templates/dynamic_body.tpl');
 ?>
